aa2a4fa926e947f44fa54c73b623b5f6467e5782
Maleesha <privatemaleesha@gmail.com>
2023-09-18 14:14:47 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel")
 const jwt = require('jsonwebtoken');
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+    return jwt.sign({_id}, process.env.SECRET, {})
 }
 
 const signUp = async (req, res) => {
